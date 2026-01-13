4d46661fd55bfa4e1e7b24774850d9aa33a435c4
Ivo Raedts <ivoraedts@hotmail.com>
2026-01-13 12:00:58 +0100

    Some improvements. And now it works fine when running local MudBlazor connecting to docker database. However running on Docker, there is something wrong with MudBlazor Server to Client or so...

--- MyApplication.Client/Models/CalendarEvent.cs
+++ MyApplication.Client/Models/CalendarEvent.cs
@@ -3,7 +3,7 @@ namespace MyApplication.Client.Models
     public class CalendarEvent
     {
         public int Id {get; set;}
-        public string Title {get; set;}
-        public string Description {get; set;}
+        public required string Title {get; set;} = string.Empty;
+        public required string Description {get; set;} = string.Empty;
     }
 }
