c1eb14db68f1f9ec8ac0b60914174fc8304ce0bb
Ivo Raedts <ivoraedts@hotmail.com>
2026-01-23 12:16:22 +0100

    Added remaining logical functions for deleting and editing items.

--- MyApplication/Controllers/CalendarEventController.cs
+++ MyApplication/Controllers/CalendarEventController.cs
@@ -15,7 +15,19 @@ public class CalendarEventController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<CalendarEvent>>> CalendarEvents()
     {
-        return await _context.CalendarEvents.AsAsyncEnumerable().ToListAsync();
+        return await _context.CalendarEvents.AsAsyncEnumerable().OrderBy(e => e.Id).ToListAsync();
+    }
+
+    // GET: api/Values/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CalendarEvent>> GetCalendarEvent(int id)
+    {
+        var calendarEvent = await _context.CalendarEvents.FindAsync(id);
+        if (calendarEvent == null)
+        {
+            return NotFound();
+        }
+        return Ok(calendarEvent);
     }
 
     // POST: api/Values
@@ -25,5 +37,50 @@ public class CalendarEventController : ControllerBase
         _context.CalendarEvents.Add(calendarEvent);
         await _context.SaveChangesAsync();
         return Ok(calendarEvent);
+    }
+
+    // PUT: api/Values/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutCalendarEvent(int id, CalendarEvent calendarEvent)
+    {
+        if (id != calendarEvent.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(calendarEvent).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return Ok(calendarEvent);
+        }
+        catch (Microsoft.EntityFrameworkCore.DbUpdateConcurrencyException)
+        {
+            if (!_context.CalendarEvents.Any(e => e.Id == id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+
+    // DELETE: api/Values/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCalendarEvent(int id)
+    {
+        var calendarEvent = await _context.CalendarEvents.FindAsync(id);
+        if (calendarEvent == null)
+        {
+            return NotFound();
+        }
+
+        _context.CalendarEvents.Remove(calendarEvent);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
